2d59aca1a75879c9af5393b5f3d154d2ea7b66be
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2024-11-29 13:54:32 -0500

    Updated to scale with state size

--- state_id_2_state.m
+++ state_id_2_state.m
@@ -3,4 +3,4 @@ function s = state_id_2_state(idx,rows,cols)
 fact_size = zeros([1,rows*cols])+3; %3 instantiations (0,1,2)
 full_fact = fullfact(fact_size)-1;
 
-s = reshape(full_fact(idx,:),[2,2]);
+s = reshape(full_fact(idx,:),[rows,cols]);
